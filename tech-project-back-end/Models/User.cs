0b50b8dc713bcf198943dfef3c5341d15a26e012
Kiet Nguyen <giakietnt2205@gmail.com>
2023-11-15 15:32:28 +0700

    validate a user

--- tech-project-back-end/Models/User.cs
+++ tech-project-back-end/Models/User.cs
@@ -19,16 +19,19 @@ namespace tech_project_back_end.Models
         [Required]
         [Column(TypeName = "varchar(100)")]
         [StringLength(100)]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email address.")]
         public string email { get; set; }
 
         [Required]
         [Column(TypeName = "varchar(11)")]
         [StringLength(11 | 10)]
+        [RegularExpression(@"^0[1-9][0-9]{8}$", ErrorMessage = "Invalid phone number.")]
         public string phone { get; set; }
 
         [Required]
         [Column(TypeName = "varchar(100)")]
         [StringLength(100)]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
         public string password { get; set; }
 
         [Required]
--- tech-project-back-end/Models/UserLogin.cs
+++ tech-project-back-end/Models/UserLogin.cs
@@ -1,8 +1,21 @@
-﻿namespace tech_project_back_end.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace tech_project_back_end.Models
 {
     public class UserLogin
     {
+        [Required]
+        [Column(TypeName = "varchar(11)")]
+        [StringLength(11 | 10)]
+        [RegularExpression(@"^0[1-9][0-9]{8}$", ErrorMessage = "Invalid phone number.")]
         public string phone { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        [StringLength(100)]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
         public string password { get; set; }
 
     }
