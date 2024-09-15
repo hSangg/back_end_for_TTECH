0af256d18ee6630682b42c2aad68ce85acbdc799
hoai sang <hoai.sang050@gmail.com>
2024-09-15 22:48:07 +0700

    update gitignore

--- tech-project-back-end/obj/Debug/net7.0/tech-project-back-end.AssemblyInfo.cs
+++ tech-project-back-end/obj/Debug/net7.0/tech-project-back-end.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tech-project-back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46b0e73b86fb6652f228c34774d9befc9cf44e2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6bd83659c2b3e8f01d3d34d58dfd3db95a96382")]
 [assembly: System.Reflection.AssemblyProductAttribute("tech-project-back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tech-project-back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
