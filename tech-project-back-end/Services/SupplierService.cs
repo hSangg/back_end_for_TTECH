441cebb5df2864b350916f8587ad08266e599aab
hoai sang <hoai.sang050@gmail.com>
2024-09-26 22:18:14 +0700

    refactor, clean code on Supplier

--- tech-project-back-end/Services/SupplierService.cs
+++ tech-project-back-end/Services/SupplierService.cs
@@ -12,37 +12,48 @@ namespace tech_project_back_end.Services
 {
     public class SupplierService : ISupplierService
     {
-        private readonly AppDbContext _appDbContext;
         private readonly IMapper _mapper;
         private readonly ISupplierRepository _supplierRepository;
         private readonly ILogger<SupplierService> _logger;
 
-        public SupplierService(AppDbContext appDbContext, IMapper mapper, ILogger<SupplierService> logger, ISupplierRepository supplierRepository)
+        public SupplierService(IMapper mapper, ILogger<SupplierService> logger, ISupplierRepository supplierRepository)
         {
-            this._appDbContext = appDbContext;
             this._mapper = mapper;
             this._logger = logger;
             this._supplierRepository = supplierRepository;
         }
 
-        public async Task<SupplierDTO> CreateSupplier(SupplierDTO dto)
+        public async Task<SupplierDTO?> CreateSupplier(SupplierDTO dto)
         {
             try
             {
                 var supplier = await _supplierRepository.Create(dto);
-                return _mapper.Map<SupplierDTO>(supplier);
+                return supplier != null
+                    ? _mapper.Map<SupplierDTO>(supplier)
+                    : null;
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Error while adding supplier");
-                return null;
+                _logger.LogError(ex, "Error while creating supplier");
+                throw;
             }
         }
 
-        public async Task<SupplierDTO> DeleteSupplierById(string id)
+        public async Task<SupplierDTO?> DeleteSupplierById(string id)
         {
-            var supplier = await _supplierRepository.Delete(id);
-            return _mapper.Map<SupplierDTO>(supplier);
+            try
+            {
+                var supplier = await _supplierRepository.Delete(id);
+                return supplier != null
+                    ? _mapper.Map<SupplierDTO>(supplier)
+                    : null;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error while creating supplier");
+                throw;
+            }
+            
         }
 
         public async Task<List<SupplierDTO>> GetAllSupplier()
@@ -50,28 +61,46 @@ namespace tech_project_back_end.Services
             try
             {
                 var supplierList = await _supplierRepository.GetAll();
-                var supplierDTOList = _mapper.Map<List<SupplierDTO>>(supplierList);
-                return supplierDTOList;
+                return _mapper.Map<List<SupplierDTO>>(supplierList);
             }
             catch (Exception ex) 
             {
                 _logger.LogError(ex, "Error while adding supplier");
-                return null;
+                throw;
             }
         }
 
-        public async Task<SupplierDTO> GetSupplierById(string id)
+        public async Task<SupplierDTO?> GetSupplierById(string id)
         {
-            var supplier = await _supplierRepository.GetById(id);
+            try
+            {
+                var supplier = await _supplierRepository.GetById(id);
+                return supplier != null
+                    ? _mapper.Map<SupplierDTO?>(supplier)
+                    : null;
+            }
+            catch
+            {
+                _logger.LogError("Error while fetching supplier with id: ", id);
+                throw;
+            }
 
-            var supplierDTO = _mapper.Map<SupplierDTO>(supplier);
-            return supplierDTO;
         }
 
-        public async Task<SupplierDTO> UpdateSupplier(SupplierDTO dto)
+        public async Task<SupplierDTO?> UpdateSupplier(SupplierDTO dto)
         {
-            var supplierDTO = await _supplierRepository.Update(dto);
-            return dto;
+            try
+            {
+                var supplier = await _supplierRepository.Update(dto);
+                return supplier != null 
+                    ? _mapper.Map<SupplierDTO>(supplier) 
+                    : null;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while updating supplier with ID {SupplierId}", dto.SupplierId);
+                throw;
+            }
         }
     }
 }
